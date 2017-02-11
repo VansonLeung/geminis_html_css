313d66e0385256584e268701464a1aee00536791
VanVan 仔 <VanVan >
2017-02-11 16:52:02 +0800

    Add Messsage receiver, fix popup

--- js/init.js
+++ js/init.js
@@ -6,3 +6,17 @@ var iframeLoaded = function(e)
 {
   console.log(e);
 }
+
+// This will popup any html URLs with given height
+window.popupIframe = function(url, height)
+{
+	var p = new Popelt({
+	    contentType: 'iframe',
+	    loadUrl: url,
+	    iframeWidth: '100%',
+	    iframeHeight: height || '500px'
+	});
+	// p.addCloseButton();
+	p.show();
+
+}
